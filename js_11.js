65716a88c862a42c80de4d2734f93a9d7e2fffc7
Razzzmes <115660513+Razzzmes@users.noreply.github.com>
2023-01-13 18:28:38 +0700

    Update js_11.js

--- js_11.js
+++ js_11.js
@@ -1,22 +1,42 @@
 class Worker {
     
-    #firstName
-    #lastName
-    #rate
-    #days
+    #firstName;
+    #lastName;
+    #rate;
+    #days;
 
     constructor(firstName, lastName, rate, days){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.rate = rate;
-        this.days = days;
+        this.#firstName = firstName;
+        this.#lastName = lastName;
+        this.#rate = rate;
+        this.#days = days;
+    }
+
+    getFirstName() {
+        return this.#firstName;
+    }
+
+    getLastName() {
+        return this.#lastName;
+    }
+
+    getRate() {
+        return this.#rate;
+    }
+    
+    getDays() {
+        return this.#days;
     }
     
     getSalary(){
-        return "Имя работника: " + this.firstName + " " + this.lastName + ", "+ "Зарплата: " + this.rate*this.days
+        return this.#rate * this.#days
     }
 }
 
 
 let worker = new Worker('Павел', 'Миронов', '1000', '30')
-console.log (worker.getSalary())
+console.log(worker.getFirstName());
+console.log(worker.getLastName());
+console.log(worker.getRate());
+console.log(worker.getDays());
+console.log(worker.getSalary());
