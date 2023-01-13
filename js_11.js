2eaee897b11e86d8887428a4fcb6df00a262f46e
Razzzmes <115660513+Razzzmes@users.noreply.github.com>
2023-01-13 18:38:30 +0700

    Update js_11.js

--- js_11.js
+++ js_11.js
@@ -1,42 +1,19 @@
 class Worker {
     
-    #firstName;
-    #lastName;
-    #rate;
-    #days;
-
     constructor(firstName, lastName, rate, days){
-        this.#firstName = firstName;
-        this.#lastName = lastName;
-        this.#rate = rate;
-        this.#days = days;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.rate = rate;
+        this.days = days;
     }
-
-    getFirstName() {
-        return this.#firstName;
-    }
-
-    getLastName() {
-        return this.#lastName;
-    }
-
-    getRate() {
-        return this.#rate;
-    }
-    
-    getDays() {
-        return this.#days;
-    }
-    
     getSalary(){
-        return this.#rate * this.#days
+        console.log("Имя работника:" + this.firstName + " " + this.lastName + ", " +"Зарплата: " + this.rate*this.days)
     }
 }
 
 
-let worker = new Worker('Павел', 'Миронов', '1000', '30')
-console.log(worker.getFirstName());
-console.log(worker.getLastName());
-console.log(worker.getRate());
-console.log(worker.getDays());
-console.log(worker.getSalary());
+let worker = new Worker('Павел', 'Миронов', '1000', '30',)
+worker.getSalary()
+
+let worker2 = new Worker('Саша', 'Кос', '100', '30',)
+worker2.getSalary()
