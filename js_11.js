5fb4d3386a8281220521cbdc83d35d63465eabfa
Razzzmes <115660513+Razzzmes@users.noreply.github.com>
2023-01-13 17:53:36 +0700

    Add files via upload

--- js_11.js
+++ js_11.js
@@ -1,23 +1,22 @@
 class Worker {
+    
+    #firstName
+    #lastName
+    #rate
+    #days
+
     constructor(firstName, lastName, rate, days){
         this.firstName = firstName;
         this.lastName = lastName;
         this.rate = rate;
         this.days = days;
     }
-    sayName(){
-        console.log(`Имя работника: ${this.firstName} ${this.lastName}`)
-    }
+    
     getSalary(){
-        console.log("Зарплата: " + this.rate*this.days)
+        return "Имя работника: " + this.firstName + " " + this.lastName + ", "+ "Зарплата: " + this.rate*this.days
     }
 }
 
 
-let worker = new Worker('Павел', 'Миронов', '1000', '30',)
-worker.sayName()
-worker.getSalary()
-
-let worker2 = new Worker('Саша', 'Кос', '100', '30',)
-worker2.sayName()
-worker2.getSalary()
+let worker = new Worker('Павел', 'Миронов', '1000', '30')
+console.log (worker.getSalary())
