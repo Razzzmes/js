f04bfe5d72a084d4eb7656dfbe3c30f70a7bf5f1
Razzzmes <115660513+Razzzmes@users.noreply.github.com>
2023-01-16 14:09:21 +0700

    Update js_8.js

--- js_8.js
+++ js_8.js
@@ -1,7 +1,37 @@
-let array = [
+let arr = [
     [1,4,5],
     [1,3,4],
     [2,6]
 ]
 
-console.log(array.flat())
+array = arr.flat()
+
+function quickSort(array){
+
+    if (array.length <= 1){
+        return array
+    }
+
+    let pivotIndex = Math.floor(array.length / 2);
+    let pivot = array[pivotIndex]
+    let less = []
+    let greater = []
+
+    for(let i = 0; i < array.length; i++){
+
+        if (i === pivotIndex){
+            continue
+        }
+
+        if(array[i] < pivot){
+            less.push(array[i])
+        }
+
+        else{
+            greater.push(array[i])
+        }
+    }
+    return [...quickSort(less), pivot, ...quickSort(greater)]
+}
+
+console.log(quickSort(array))
